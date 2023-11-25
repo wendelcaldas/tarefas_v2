342e4dcc3dab98041e6632b009260f3702c4ab4d
Wendel <wendelsouza.dev@gmail.com>
2023-11-25 19:46:10 -0300

    comitdevelop

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './components/HelloWorld';
+
+function App() {
+
+  const name = "wendel"
+
+  return (
+    <div className="App">
+      <HelloWorld/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Frase.js
@@ -0,0 +1,9 @@
+function Frase(){
+    return(
+        <div>
+            <p>Componente de frase</p>
+        </div>
+    )
+}
+
+export default Frase
